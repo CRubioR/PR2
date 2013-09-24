6ef5639868cbe76de0080a03c72cba6b08575a89
aluadm <aluadm@ORD-COI-SIM-19>
2013-09-24 21:16:36 +0200

    Segon Commit

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -12,6 +12,6 @@ public class JavaApplication3 {
 
  
     public static void main(String[] args) {
-        System.out.println("Adios mundo");
+        System.out.println("Adios mundo cruel");
     }
 }
